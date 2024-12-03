dbfe72b1cd775fa43aacfbd69216e629e5e3e76e
SumeyyaNacar <sumeyyanacar08@gmail.com>
2024-12-03 11:24:40 +0300

    answer:  4th & 5th question and new question added

--- javaPractice/PracticeMain.java
+++ javaPractice/PracticeMain.java
@@ -17,8 +17,9 @@ public class PracticeMain {
 //        System.out.println(area(5.2));
 //        System.out.println(area(2.4,4.7));
 //        printEqual(1,3,2);
-
+//        PracticeAnswers.calculate(0,5);
 //        PracticeAnswers.isPalindrome2(1232323);
+        PracticeAnswers.checkPallindrome(15541);
 
 
 
