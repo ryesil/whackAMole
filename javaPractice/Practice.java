e02fd541369d41cbf6b72db1b478656e82682453
ryesil <ramazanysl@gmail.com>
2024-12-02 19:08:21 -0600

    question 5

--- javaPractice/Practice.java
+++ javaPractice/Practice.java
@@ -155,5 +155,15 @@ public static void printEqual2(int a, int b, int c){
      */
 
 
-
+//Question 5
+//Number Palindrome
+//    Write a method called isPalindrome with one int parameter called number.
+//    The method needs to return a boolean.
+//    It should return true if the number is a palindrome number otherwise it should return false.
+//    Check the tips below for more info about palindromes.
+//    Example Input/Output
+//    isPalindrome(-1221); → should return true
+//    isPalindrome(707); → should return true
+//    isPalindrome(11212); → should return false because reverse is 21211 and that is not equal to 11212.
+//    Tip: What is a Palindrome number?  A palindrome number is a number which when reversed is equal to the original number. For example: 121, 12321, 1001 etc.
 }
