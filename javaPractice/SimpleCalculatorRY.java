2603fd273f1172ad38042e169db7bd8f086e3b6f
ryesil <ramazanysl@gmail.com>
2024-12-16 22:55:07 -0600

    Answers to Q16 and Q17 added.

---
+++ javaPractice/SimpleCalculatorRY.java
@@ -0,0 +1,49 @@
+package javaPractice;
+
+public class SimpleCalculatorRY {
+    private double firstNumber;
+    private double secondNumber;
+
+    public SimpleCalculatorRY() {
+    }
+
+    public SimpleCalculatorRY(double firstNumber, double secondNumber) {
+        this.firstNumber = firstNumber;
+        this.secondNumber = secondNumber;
+    }
+
+    public double getFirstNumber() {
+        return this.firstNumber;
+    }
+
+    public double getSecondNumber() {
+        return this.secondNumber;
+    }
+
+    public void setFirstNumber(double firstNumber) {
+        this.firstNumber = firstNumber;
+    }
+
+    public void setSecondNumber(double secondNumber) {
+        this.secondNumber = secondNumber;
+    }
+
+    public double getAdditionResult() {
+        return (this.firstNumber + this.secondNumber);
+    }
+
+    public double getSubtractionResult() {
+        return (this.firstNumber - this.secondNumber);
+    }
+
+    public double getMultiplicationResult() {
+        return (this.firstNumber * this.secondNumber);
+    }
+
+    public double getDivisionResult() {
+        if (secondNumber == 0) {
+            return 0;
+        }
+        return (this.firstNumber / this.secondNumber);
+    }
+}
