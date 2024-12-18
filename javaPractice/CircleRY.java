50bd12328d897697e77e60f164cf667700bf823a
Ramazan Yesil <ramazanysl@gmail.com>
2024-12-17 19:32:23 -0600

    asd

---
+++ javaPractice/CircleRY.java
@@ -0,0 +1,19 @@
+package javaPractice;
+
+public class CircleRY {
+    private double radius;
+
+    CircleRY(double radius) {
+        if(radius < 0){
+            this.radius = 0;
+        } else {
+            this.radius = radius;
+        }
+    }
+
+public double getRadius() { return radius; }
+
+    public double getArea() {return Math.PI * radius * radius; }
+
+
+}
