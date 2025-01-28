5208df2a8cbb52bbf16d7f0e77f0361aa8b0d1dd
ryesil <ramazanysl@gmail.com>
2025-01-27 20:58:31 -0600

    29 ve 30 cu sorular eklendi

--- javaPractice/PracticeQuestions.java
+++ javaPractice/PracticeQuestions.java
@@ -582,4 +582,57 @@ The method should first print out the newly passed in array as Array = [1, 2, 3,
 and then once it's been reversed, print it out as Reversed array = [5, 4, 3, 2, 1]
  */
 
+    /*
+Q29
+    It's now time for an ArrayList challenge. The challenge is to create an interactive
+    console program, with a menu of options as shown here, and functionality that I will describe now.
+    Using the Scanner class, solicit a menu choice, 0,
+            1, or 2, and process the item accordingly. 0 to quit the program.
+            1 - prompt the user for a comma-delimited list of items to add to a list.
+    And similarly, if the user enters 2, prompt them for a list, again comma-delimited,
+    to remove from the same list. The list will be a grocery
+    list and should be an ArrayList. You should use methods on the ArrayList
+    to add items, remove items, check if an item is already in the list, and print a sorted list.
+    You should print the list, sorted alphabetically, after each operation.
+    You shouldn't allow duplicate items in the list. So, that's the challenge.
+    See how you get on with it. Best of luck.
+ */
+
+ /*
+ Q30
+ Mobile Phone
+Create a program that implements a simple mobile phone with the following capabilities.
+1.  Implement the master class MobilePhone, that holds the ArrayList of Contacts, with the following attributes:
+    -  Two fields, a String called myNumber and an ArrayList of type Contact called myContacts.
+    -  A constructor that takes a String (the phone number) and initialises myNumber and instantiates myContacts.
+    -  And seven methods, they are (their functions are in their names):
+        -  addNewContact(), has one parameter of type Contact and returns a boolean. Returns true if the contact doesn't exists, or false if the contact already exists.
+        -  updateContact(), has two parameters of type Contact (the old contact that will be updated with the new contact) and returns a boolean. Returns true if the contact exists and was updated successfully, or false if the contact doesn't exists.
+        -  removeContact(), has one parameter of type Contact and returns a boolean. Returns true if the contact exists and was removed successfully, or false if the contact doesn't exists.
+        -  findContact(), has one parameter of type Contact and returns an int. The returned value is it's position in the ArrayList, it will either be -1 (doesn't exists) or a value greater than or equal to 0 (does exists).
+        -  findContact(), same as above, only it has one parameter of type String.
+        -  queryContact(), has one parameter of type String and returns a Contact. Use the String to search for the name and then return the Contact. Return null otherwise.
+        -  printContacts(), has no parameters and doesn't return anything. Print the contacts in the following format:
+Contact List:
+1. Bob -> 31415926
+2. Alice -> 16180339
+3. Tom -> 11235813
+4. Jane -> 23571113
+2. Implement the Contact class with the following attributes:
+    -  Two fields, both String, one called name and the other phoneNumber.
+    -  A constructor that takes two Strings, and initialises name and phoneNumber.
+    -  And Three methods, they are:
+        -  getName(), getter for name.
+        -  getPhoneNumber(), getter for phoneNumber.
+        -  createContact(), has two parameters of type String (the persons name and phone number) and returns an instance of Contact. This is the only method that is static.
+TIP:  In MobilePhone, use findContact() in the other methods (except printContacts()) to check if it exists before proceeding.
+TIP:  Two Contact objects are equal if they have the same name.
+TIP:  Be extremely careful about spaces in the printed message.
+NOTE:  All fields are private.
+NOTE:  Constructors should be defined as public.
+NOTE:  All methods should be defined as public (except for the two findContact() methods which are private).
+  */
+
+
+
 }
