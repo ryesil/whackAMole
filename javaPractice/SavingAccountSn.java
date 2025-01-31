4a3eb44e03d3d03fcf405044ecc75ebda8d9ef89
SumeyyaNacar <sumeyyanacar08@gmail.com>
2025-01-31 13:19:50 +0300

    Sn java 21 (#30)
    * new questions added
    
    * new question added
    
    * update
    
    * update
    
    * answer of Q21

---
+++ javaPractice/SavingAccountSn.java
@@ -0,0 +1,17 @@
+package javaPractice;
+
+public class SavingAccountSn extends BankAccountSn{
+    private double interestRate;
+
+    // Constructor
+    public SavingAccountSn(String accountNumber, double balance, double interestRate) {
+        super(accountNumber, balance);
+        this.interestRate = interestRate;
+    }
+
+    // Calculate interest
+    public double calculateInterest() {
+        return getBalance() * interestRate;
+    }
+}
+
