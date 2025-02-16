fdffe2f719f9c02e95206f0fdb17e381bb5ee851
ryesil <ramazanysl@gmail.com>
2025-02-15 21:22:38 -0600

    Q31 and Q32 are added. Good luck.

--- javaPractice/PracticeQuestions.java
+++ javaPractice/PracticeQuestions.java
@@ -633,6 +633,87 @@ NOTE:  Constructors should be defined as public.
 NOTE:  All methods should be defined as public (except for the two findContact() methods which are private).
   */
 
+/*
+    LinkList
+    Q31
+    Playlist
+Create a program that implements a playlist of songs.
+To start you off, implement the following classes:
+1.  Album
+    -  It has three fields, two Strings called name and artist, and an ArrayList that holds objects of type Song called songs.
+    -  A constructor that accepts two Strings (name of the album and artist). It initialises the fields and instantiates songs.
+    -  And three methods, they are:
+        -  addSong(), has two parameters of type String (title of song), double (duration of song) and returns a boolean. Returns true if the song was added successfully or false otherwise.
+        -  findSong(), has one parameter of type String (title of song) and returns a Song. Returns the Song if it exists, null if it doesn't exists.
+        -  addToPlayList(), has two parameters of type int (track number of song in album) and LinkedList (the playlist) that holds objects of type Song, and returns a boolean. Returns true if it exists and it was added successfully using the track number, or false otherwise.
+        -  addToPlayList(), has two parameters of type String (title of song) and LinkedList (the playlist) that holds objects of type Song, and returns a boolean. Returns true if it exists and it was added successfully using the name of the song, or false otherwise.
+2.  Song
+    -   It has two fields, a String called title and a double called duration.
+    -  A constructor that accepts a String (title of the song) and a double (duration of the song). It initialises title and duration.
+    -  And two methods, they are:
+        -  getTitle(), getter for title.
+        -  toString(), Songs overriding toString method. Returns a String in the following format: "title: duration"
+->  SAMPLE INPUT
+ArrayList<Album> albums = new ArrayList<>();
+Album album = new Album("Stormbringer", "Deep Purple");
+album.addSong("Stormbringer", 4.6);
+album.addSong("Love don't mean a thing", 4.22);
+album.addSong("Holy man", 4.3);
+album.addSong("Hold on", 5.6);
+album.addSong("Lady double dealer", 3.21);
+album.addSong("You can't do it right", 6.23);
+album.addSong("High ball shooter", 4.27);
+album.addSong("The gypsy", 4.2);
+album.addSong("Soldier of fortune", 3.13);
+albums.add(album);
+album = new Album("For those about to rock", "AC/DC");
+album.addSong("For those about to rock", 5.44);
+album.addSong("I put the finger on you", 3.25);
+album.addSong("Lets go", 3.45);
+album.addSong("Inject the venom", 3.33);
+album.addSong("Snowballed", 4.51);
+album.addSong("Evil walks", 3.45);
+album.addSong("C.O.D.", 5.25);
+album.addSong("Breaking the rules", 5.32);
+album.addSong("Night of the long knives", 5.12);
+albums.add(album);
+LinkedList<Song> playList = new LinkedList<Song>();
+albums.get(0).addToPlayList("You can't do it right", playList);
+albums.get(0).addToPlayList("Holy man", playList);
+albums.get(0).addToPlayList("Speed king", playList);  // Does not exist
+albums.get(0).addToPlayList(9, playList);
+albums.get(1).addToPlayList(3, playList);
+albums.get(1).addToPlayList(2, playList);
+albums.get(1).addToPlayList(24, playList);  // There is no track 24
+TIP:  In Album, use the findSong() method in addSong() and addToPlayList(String, LinkedList) to check if a song exists before proceeding.
+TIP:  Be extremely careful with the spelling of the names of the fields, constructors and methods.
+TIP:  Be extremely careful about spaces and spelling in the returned String from the toString() method.
+NOTE:  All fields are private.
+NOTE:  All constructors are public.
+NOTE:  All methods are public (except for findSong() which is private).
+ */
+
+/*
+    Q32
+    AutoBoxing and Unboxing
+    Okay, so it's time for a challenge on autoboxing and unboxing.
+In this challenge, you'll need to create a simple banking application with a Customer and Bank type.
+The Customer will have a name and an ArrayList of transactions containing Double wrapper elements.
+A customer's transaction can be a credit, which means a positive amount, or it can be
+a debit, a negative amount. The Bank will have a name,
+and an ArrayList of customers. The bank should add a new customer,
+if they're not yet already in the list. The bank class should allow a customer to add a
+transaction, to an existing Customer. This class should also print a statement, that
+includes the customer name, and the transaction amounts. This method should use unboxing.
+Hopefully that's fairly clear but also gives you a little room to be creative.
+Alright, so it's your turn to see if you can complete the challenge.
+Remember don't overcomplicate things, you should aim to keep it pretty basic.
+The customer's literally only the name and the transactions at this stage.
+You don't need to store customer contract addresses or anything like that.
+The main thing is the relationship between the various classes and also
+the auto boxing and unboxing.
+ */
+
 
 
 }
