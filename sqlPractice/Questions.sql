b9484cac448d63354e2ae2590982aa3e74c1e1c5
ryesil <ramazanysl@gmail.com>
2025-02-22 20:28:57 -0600

    Sql Questions added

---
+++ sqlPractice/Questions.sql
@@ -0,0 +1,17 @@
+--Questions
+-- (Please open a folder under your name or initials. Afterwards, copy and paste the questions into a new file inside the folder to answer).
+
+
+--Q1
+-- write a query in SQL to retrieve all rows and columns from the employee table in the Adventureworks database.
+-- Sort the result set in ascending order on jobtitle.
+
+--Q2
+-- write a query in SQL to retrieve all rows and columns from the employee table using table aliasing in the Adventureworks database.
+-- Sort the output in ascending order on lastname.
+
+--Q3
+-- write a query in SQL to return all rows and a subset of the columns (FirstName, LastName, businessentityid)
+-- from the person table in the AdventureWorks database. The third column heading is renamed to Employee_id.
+-- Arranged the output in ascending order by lastname.
+
